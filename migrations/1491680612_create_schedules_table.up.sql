2d0bf43e15d87fd6addf1b627264554cf4b2aa01
mgadzhi <musaevgadzhi@gmail.com>
2017-04-08 22:37:36 +0200

    Make (subreddit, slack_chat) unique

--- migrations/1491680612_create_schedules_table.up.sql
+++ migrations/1491680612_create_schedules_table.up.sql
@@ -4,7 +4,8 @@ CREATE TABLE schedules (
        submissions_num integer,
        subreddit varchar(20),
        slack_chat varchar(128),
-       last_posted_at timestamp
+       last_posted_at timestamp,
+       unique (subreddit, slack_chat)
 );
 
 CREATE INDEX last_posted_index ON schedules(last_posted_at);
